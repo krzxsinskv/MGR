8905216e17f2f18e0c75f707f7101d388551e089
bkrzesin <01149657@pw.edu.pl>
2025-12-08 20:54:35 +0100

    Added ffmap.py

---
+++ src/model/ffmap.py
@@ -0,0 +1,24 @@
+import sys
+import os
+import torch
+from pathlib import Path
+
+project_dir = Path(__file__).resolve().parents[2]
+sys.path.insert(1, os.path.join(sys.path[0], project_dir))
+from src.model.models_architectures import MODEL_ARCHITECTURES
+from src.common.other_utils import extract_fusion_features, plot_fusion_feature_map, extract_timestamp, create_input_sample, load_case_config
+
+if __name__ == '__main__':
+    config = load_case_config(case_number=1)
+    model = MODEL_ARCHITECTURES[config["model"]["type"]]()
+    model_path = config["evaluation"]["model"]
+    model.load_state_dict(torch.load(model_path, map_location="cpu"))
+
+    sample = create_input_sample()
+    sample = torch.tensor(sample, dtype=torch.float32)
+    sample = sample.unsqueeze(0).unsqueeze(0)
+
+    fusion = extract_fusion_features(model, sample)
+    timestamp = extract_timestamp(model_path=model_path)
+    plot_fusion_feature_map(fusion_features=fusion, timestamp=timestamp, save=True)
+
